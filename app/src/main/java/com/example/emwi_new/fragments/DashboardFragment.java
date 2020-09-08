e7d90dae08ff4cd7d654d550e578392de1c7e403
kalyani <kalyanipawar94@gmail.com>
2020-09-08 10:43:11 +0530

    Dahsboard work
    Added dashborad fragment and its view model.

---
+++ app/src/main/java/com/example/emwi_new/fragments/DashboardFragment.java
@@ -0,0 +1,6 @@
+package com.example.emwi_new.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class DashboardFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/emwi_new/viewmodel/DashboardViewModel.java
@@ -0,0 +1,6 @@
+package com.example.emwi_new.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+}
