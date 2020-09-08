dd2f4d2a520d489552a274753c62fa3c5b2cd473
kalyani <kalyanipawar94@gmail.com>
2020-09-08 10:54:15 +0530

    Strings xml file
    Added new string values.

--- app/src/main/java/com/example/emwi_new/fragments/DashboardFragment.java
+++ app/src/main/java/com/example/emwi_new/fragments/DashboardFragment.java
@@ -1,6 +1,37 @@
 package com.example.emwi_new.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.emwi_new.R;
+import com.example.emwi_new.ViewClasses.Activities.ui.gallery.GalleryViewModel;
+import com.example.emwi_new.viewmodel.DashboardViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        //final TextView textView = root.findViewById(R.id.text_gallery);
+        /*dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        return root;
+    }
 }
